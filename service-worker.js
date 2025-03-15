54facec54d28367bac1f4f2db5e7564b0fdd3e0a
equipment-track <nafil.8895.s@gmail.com>
2025-03-15 12:32:59 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,6 @@
+// service-worker.js
 self.addEventListener('install', (event) => {
-    console.log("Service Worker installing...");
+    console.log('Service Worker installing.');
     event.waitUntil(
         caches.open('my-cache').then((cache) => {
             return cache.addAll([
@@ -7,24 +8,19 @@ self.addEventListener('install', (event) => {
                 '/index.html',
                 '/style.css',
                 '/script.js',
-                '/manifest.json',
-                '/icon.png'
             ]);
         })
     );
 });
 
 self.addEventListener('activate', (event) => {
-    console.log("Service Worker activated.");
+    console.log('Service Worker activated.');
 });
 
-self.addEventListener('push', (event) => {
-    const options = {
-        body: event.data.text(),
-        icon: '/icon.png',
-        badge: '/badge.png'
-    };
-    event.waitUntil(
-        self.registration.showNotification('Push Notification', options)
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
     );
 });
