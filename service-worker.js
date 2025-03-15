5d4213dc4d58a646816ab707859f103b7f518485
equipment-track <nafil.8895.s@gmail.com>
2025-03-15 12:26:49 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,35 +1,29 @@
-// Install the Service Worker and Cache the assets
-self.addEventListener('install', function(event) {
+self.addEventListener('install', (event) => {
+    console.log("Service Worker installing...");
     event.waitUntil(
-        caches.open('push-notifications-cache').then(function(cache) {
+        caches.open('my-cache').then((cache) => {
             return cache.addAll([
                 '/',
                 '/index.html',
-                '/app.js',
-                '/icon-192x192.png',
-                '/icon-512x512.png',
+                '/style.css',
+                '/script.js',
+                '/manifest.json'
             ]);
         })
     );
 });
 
-// Activate the Service Worker
-self.addEventListener('activate', function(event) {
-    console.log('Service Worker activated!');
+self.addEventListener('activate', (event) => {
+    console.log("Service Worker activated.");
 });
 
-// Push event listener
-self.addEventListener('push', function(event) {
-    console.log('Push notification received:', event);
-
-    // Set default options for notification
-    let options = {
+self.addEventListener('push', (event) => {
+    const options = {
         body: event.data.text(),
-        icon: '/icon-192x192.png',
-        badge: '/icon-192x192.png',
+        icon: '/icon.png',
+        badge: '/badge.png'
     };
-
     event.waitUntil(
-        self.registration.showNotification('Test Notification', options)
+        self.registration.showNotification('Push Notification', options)
     );
 });
